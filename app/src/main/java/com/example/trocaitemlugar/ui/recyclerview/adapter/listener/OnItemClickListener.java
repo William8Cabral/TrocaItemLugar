4d004e09b695c2eaf27d2556a2a2a9c43cb5421e
William8Cabral <william8cabral@hotmail.com>
2021-03-09 15:13:26 -0300

    projeto troca item de ligar na recycle view

---
+++ app/src/main/java/com/example/trocaitemlugar/ui/recyclerview/adapter/listener/OnItemClickListener.java
@@ -0,0 +1,8 @@
+package com.example.trocaitemlugar.ui.recyclerview.adapter.listener;
+
+import com.example.trocaitemlugar.model.Nota;
+
+public interface OnItemClickListener {
+
+    void onItemClick(Nota nota, int posicao);
+}
